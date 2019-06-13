84a2b34450acbff1aedffe18f4704c96a7b58b46
palanza <palanza@enterprise-consulting.it>
2019-06-13 12:18:35 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/Template/Files/explore.ctp

--- src/Model/Entity/File.php
+++ src/Model/Entity/File.php
@@ -19,17 +19,28 @@ use Cake\ORM\Entity;
 class File extends Entity
 {
 
-    /**
-     * Fields that can be mass assigned using newEntity() or patchEntity().
-     *
-     * Note that when '*' is set to true, this allows all unspecified fields to
-     * be mass assigned. For security purposes, it is advised to set '*' to false
-     * (or remove it), and explicitly make individual fields accessible as needed.
-     *
-     * @var array
-     */
-    protected $_accessible = [
-        '*' => true,
-        'id' => false,
-    ];
+	/**
+	 * Fields that can be mass assigned using newEntity() or patchEntity().
+	 *
+	 * Note that when '*' is set to true, this allows all unspecified fields to
+	 * be mass assigned. For security purposes, it is advised to set '*' to false
+	 * (or remove it), and explicitly make individual fields accessible as needed.
+	 *
+	 * @var array
+	 */
+	protected $_accessible = [
+		'*' => true,
+		'id' => false,
+	];
+
+	protected function _setName($name)
+	{
+		return str_replace(" ", "", $name);
+	}
+
+	protected function _setOriginalFilename($original_filename)
+	{
+		return str_replace(" ", "", $original_filename);
+	}
+
 }
